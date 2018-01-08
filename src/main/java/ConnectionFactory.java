2c1f0992c53af41e24f3565f1d6e80571e2cfd08
Ayush Prashar <ayush.prashar@knoldus.in>
2018-01-08 10:36:24 +0530

    Lastest edit

--- src/main/java/ConnectionFactory.java
+++ src/main/java/ConnectionFactory.java
@@ -1,42 +1,45 @@
-import java.sql.*;
+/**
+ * Made by Ayush Prashar
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/kip";
-    private static final String uName = "root";
-    private static final String pwd = "root";
+  private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+  private static final String DB_URL = "jdbc:mysql://localhost:3306/kip";
+  private static final String uName = "root";
+  private static final String pwd = "root";
 
-    private ConnectionFactory()
-    {
-        try {
-            Class.forName(JDBC_DRIVER);
+  private ConnectionFactory() {
+    try {
+      Class.forName(JDBC_DRIVER);
 
-        }
-
-        catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
-  //  public static ConnectionFactory singleton = new ConnectionFactory();
-    public static Connection getconnection()
-    {
-        Connection cnn = null;
+  }
 
-        try {
-            System.out.println("Connection to Database !");
+  /**accessing db*/
+  public static Connection getconnection() {
+    Connection cnn = null;
 
-            cnn=DriverManager.getConnection(
-                   DB_URL,uName,pwd);
+    try {
+      System.out.println("Connection to Database !");
+
+      cnn = DriverManager.getConnection(
+          DB_URL, uName, pwd);
 
 
-        } catch (SQLException e) {
+    } catch (SQLException e) {
 
-            e.printStackTrace();
-        }
-
-        return cnn;
+      e.printStackTrace();
     }
 
+    return cnn;
+  }
 
 
 }
--- src/main/java/DbManagement.java
+++ src/main/java/DbManagement.java
@@ -2,80 +2,76 @@
 import java.sql.*;
 
 public class DbManagement {
-    public void addToDB(int first,int second,String operand ,int result){
-        Connection cnn = ConnectionFactory.getconnection();
-        String query = "INSERT INTO calculator VALUES(?,?,?,?,?,?)";
+  public void addToDB(int first, int second, String operand, int result) {
+    Connection cnn = ConnectionFactory.getconnection();
+    String query = "INSERT INTO calculator VALUES(?,?,?,?,?,?)";
 
-        //    System.out.println("Connection bhi hai yar");
-        PreparedStatement stmt=null;
-        try{
-           stmt =cnn.prepareStatement(query);
+    //    System.out.println("Connection bhi hai yar");
+    PreparedStatement stmt = null;
+    try {
+      stmt = cnn.prepareStatement(query);
 
-            stmt.setInt(1,0);
-            stmt.setDate(2,null);
-            stmt.setInt(3,first);
-            if(operand.equals("abs"))
-            {
-                stmt.setString(4,null);
-            }
-            else
-            stmt.setInt(4,second);
-            stmt.setString(5,operand);
-            stmt.setInt(6,result);
-            stmt.executeUpdate();
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
+      stmt.setInt(1, 0);
+      stmt.setDate(2, null);
+      stmt.setInt(3, first);
+      if (operand.equals("abs")) {
+        stmt.setString(4, null);
+      } else
+        stmt.setInt(4, second);
+      stmt.setString(5, operand);
+      stmt.setInt(6, result);
+      stmt.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-    public void checkByID(int id){
-        String query = "SELECT LEFT_OPERAND,RIGHT_OPERAND,OPERATOR,RESULT FROM calculator where id = ?";
-        Connection cnn = ConnectionFactory.getconnection();
-        PreparedStatement stmt;
-        try{
-            stmt = cnn.prepareStatement(query);
-            stmt.setInt(1,id);
-            ResultSet rs =stmt.executeQuery();
+  }
 
-            if(rs.next()){
-                int left = rs.getInt(1);
-                int right = rs.getInt(2);
-                String op = rs.getString(3);
-                int result = rs.getInt(4);
+  public void checkByID(int id) {
+    String query = "SELECT LEFT_OPERAND,RIGHT_OPERAND,OPERATOR,RESULT FROM calculator where id = ?";
+    Connection cnn = ConnectionFactory.getconnection();
+    PreparedStatement stmt;
+    try {
+      stmt = cnn.prepareStatement(query);
+      stmt.setInt(1, id);
+      ResultSet rs = stmt.executeQuery();
 
-
-               System.out.println("\nFirst operand "+left+" Second Operand "+right+
-                        " Operation "+op+" result "+result );
-            }
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
+      if (rs.next()) {
+        int left = rs.getInt(1);
+        int right = rs.getInt(2);
+        String op = rs.getString(3);
+        int result = rs.getInt(4);
 
 
+        System.out.println("\nFirst operand " + left + " Second Operand " + right +
+            " Operation " + op + " result " + result);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-    public void allCalcs(String operator){
-        String query = "\nSELECT LEFT_OPERAND,RIGHT_OPERAND,RESULT FROM calculator WHERE OPERATOR=?";
-        Connection cnn = ConnectionFactory.getconnection();
-        PreparedStatement stmt;
-        try{
-            stmt=cnn.prepareStatement(query);
-            stmt.setString(1,operator);
-            ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()){
-                int left = rs.getInt(1);
-                int right = rs.getInt(2);
-                int result = rs.getInt(3);
 
-                System.out.println("\nFirst operand "+left+" Second Operand "+right+
-                        " Operation "+operator+" result "+result );
+  }
 
-            }
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
+  public void allCalcs(String operator) {
+    String query = "\nSELECT LEFT_OPERAND,RIGHT_OPERAND,RESULT FROM calculator WHERE OPERATOR=?";
+    Connection cnn = ConnectionFactory.getconnection();
+    PreparedStatement stmt;
+    try {
+      stmt = cnn.prepareStatement(query);
+      stmt.setString(1, operator);
+      ResultSet rs = stmt.executeQuery();
+
+      while (rs.next()) {
+        int left = rs.getInt(1);
+        int right = rs.getInt(2);
+        int result = rs.getInt(3);
+
+        System.out.println("\nFirst operand " + left + " Second Operand " + right +
+            " Operation " + operator + " result " + result);
+
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+  }
 }
